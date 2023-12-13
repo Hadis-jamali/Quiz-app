fcad87f1aa8e9072e404925924ffc62ec39cc19f
Hadis Jamali <hadisjamali95@gmail.com>
2023-12-13 18:51:33 +0100

    add formatData function

--- js/game.js
+++ js/game.js
@@ -8,8 +8,22 @@ let formatedData = null;
 const fechData = async () => {
   const response = await fetch(URL);
   const json = await response.json();
-  formatedData = json;
+  formatedData = formatData(json.result);
+  console.log(formatedData);
   start();
+};
+
+const formatData = (questionData) => {
+  const result = questionData.map((item) => {
+    const questionObject = { question: item.question };
+    const answers = [...item.incorrect_answers];
+    const correctAnswerIndex = Math.floor(Math.random() * 4);
+    answers.splice(correctAnswerIndex, 0, item.incorrect_answers);
+    questionObject.answers = answers;
+    questionObject.correctAnswerIndex = correctAnswerIndex;
+    return questionObject;
+  });
+  return result;
 };
 
 const start = () => {
