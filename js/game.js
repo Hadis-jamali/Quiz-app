6a4877be0145aab264a5e48ea6ff909702b5fd56
Hadis Jamali <hadisjamali95@gmail.com>
2024-01-11 20:28:20 +0100

    add showQustion function

--- js/game.js
+++ js/game.js
@@ -1,11 +1,15 @@
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
+const questionText = document.getElementById("question-text");
+const answerList = document.querySelectorAll(".answer-text");
 
 const URL = "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 
 let formattedData = null;
+let questionIndex = 0;
+let correctAnswer = null;
 
-const fechData = async () => {
+const fetchData = async () => {
   const response = await fetch(URL);
   const json = await response.json();
   formattedData = formatData(json.results);
@@ -26,8 +30,23 @@ const formatData = (questionData) => {
 };
 
 const start = () => {
+  showQuestion();
   loader.style.display = "none";
   container.style.display = "block";
 };
 
-window.addEventListener("load", fechData);
+const showQuestion = () => {
+  const { question, answers, correctAnswerIndex } = formattedData[questionIndex];
+  correctAnswer = correctAnswerIndex;
+  questionText.innerText = question;
+  answerList.forEach((button, index) => {
+    button.innerText = answers[index];
+  });
+};
+
+function checkAnswer() {}
+
+window.addEventListener("load", fetchData);
+answerList.forEach((button, index) => {
+  button.addEventListener("click", checkAnswer);
+});
